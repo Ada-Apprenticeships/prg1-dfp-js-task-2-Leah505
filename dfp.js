9d47df9bf661f10097cec5948c9b6b3f94f3323b
Leah505 <leah.clarke@ada.ac.uk>
2024-10-24 18:29:14 +0000

    Docstrings

--- dfp.js
+++ dfp.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
 
+/**
+ * Parses a UTF-8 encoded text file, performs transformations on the data, and writes the results to an output file.
+ * 
+ * The function expects two mandatory parameters: the input file path and the output file path. An optional delimiter
+ * can be provided, with the default set to a semicolon (';').
+ * 
+ * It processes each line of the input file by reversing the order of the columns (sentiment;review) and trimming 
+ * the review to 20 characters. The output is saved to the specified output file.
+ * 
+ * If the input file does not exist, the function returns -1.
+ * 
+ * @param {string} inputFile - The path to the input file.
+ * @param {string} outputFile - The path to the output file.
+ * @param {string} [delimiter=';'] - The delimiter used to separate columns in the input file. Defaults to ';'.
+ * @returns {number} - The total number of records exported, or -1 if the input file does not exist.
+ */
 function parseFile(inputFile, outputFile, delimiter = ';') {
     if (!fs.existsSync(inputFile)) {
         return -1;
